140ef2e80f54b9e0ef32564c769839ddc0a0d11f
Paul Derbyshire <pderbs@icloud.com>
2021-05-23 08:44:06 +0100

    Initial Commit

---
+++ VideoTicker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VideoTicker
+//
+//  Created by Paul Derbyshire on 23/05/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
